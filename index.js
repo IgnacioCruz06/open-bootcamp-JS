dab7316dbef7cf6403674e73e08ac8ff75eb0da9
José Ignacio Cruz Reyes <117957482+IgnacioCruz06@users.noreply.github.com>
2023-02-21 01:16:17 -0600

    chore: ejercicio 1 finished

---
+++ index.js
@@ -0,0 +1,11 @@
+// Este es el primer ejercio en el curso de JavaScript Básico de Open Bootcamp
+
+/* 
+Enunciado del ejercicio:
+Crea un nuevo proyecto de Node, y dentro del package.json crea un script que muestre por pantalla "Hola, 
+este es mi primer ejercicio con Node en el mejor Bootcamp de programación del mundo".
+*/
+
+console.log(
+  "Hola, este es mi primer ejercicio con Node en el mejor Bootcamp de programación del mundo"
+);
